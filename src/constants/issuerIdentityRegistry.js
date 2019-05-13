f950ecdee9105862cde4699728b21315850fa596
James Chartrand <jc.chartrand@gmail.com>
2019-05-13 18:05:08 -0400

    update registry url to https

--- src/constants/issuerIdentityRegistry.js
+++ src/constants/issuerIdentityRegistry.js
@@ -1,6 +1,6 @@
 //const mcMasterIdentity = 'https://raw.githubusercontent.com/jchartrand/blockcertsTests/master/mainnet/issuer.json';
 
-const ISSUER_IDENTITY_REGISTRY = "http://raw.githubusercontent.com/jchartrand/blockcertsTests/master/issuerIdRegistry.json"
+const ISSUER_IDENTITY_REGISTRY = "https://raw.githubusercontent.com/jchartrand/blockcertsTests/master/issuerIdRegistry.json"
 
 export  {
 	ISSUER_IDENTITY_REGISTRY
